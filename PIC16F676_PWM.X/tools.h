f5b0d5bd2b36aedb665b2516c5d3d785454634aa
Achim <35463379+AchD@users.noreply.github.com>
2022-01-28 17:55:11 +0100

    Delete PIC16F676_PWM.X directory

--- PIC16F676_PWM.X/tools.h
+++
@@ -1,8 +0,0 @@
-/*
- *  Tools für C
- */
-
-#define BIT_READ(var, bitno)    (var & (1 << bitno))
-#define BIT_RESET(var, bitno)   (var &= ~(1 << bitno))
-#define BIT_SET(var, bitno)     (var |= 1 << bitno)
-#define BYTE2WORD(low, high)    (low + (high<<8))
